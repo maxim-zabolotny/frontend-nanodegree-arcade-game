ab7b7ac58b2d056612ed3519e5ad50c2f99815fb
maxim zabolotnyi <maxim.zabolotny20121776@gmail.com>
2021-01-03 01:01:09 +0200

    frogger

--- js/app.js
+++ js/app.js
@@ -1,46 +1,119 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+const START = {
+    X : 200,
+    Y : 390
+};
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+const WINDOW_WIDTH = 505;
+
+const STEP = {
+    X : 100,
+    Y : 80
+};
+
+const Enemy = function(x,y,speed, player) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.player = player;
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    if(this.x >= WINDOW_WIDTH)
+        this.x = -STEP.X;
+    else
+        this.x += dt * this.speed;
+
+    this.checkCollision();
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+Enemy.prototype.checkCollision = function() {
+    if (this.player.x < this.x + STEP.X/2 &&
+        this.player.x + STEP.X/2 > this.x &&
+        this.player.y < this.y + STEP.Y/4 &&
+        this.player.y + STEP.Y/4 > this.y) {
+        this.player.x = START.X;
+        this.player.y = START.Y;
+    }
+}
 
+const Player = function(){
+    this.x = START.X;
+    this.y = START.Y;
+    this.sprite = 'images/char-boy.png';
+};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.update = function() {
 
+};
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+Player.prototype.handleInput = function(key){
+    switch (key) {
+        case 'left':
+            this.x -= STEP.X;
+            if(this.x < 0){
+                this.x = 0;
+            }
+            break;
+        case 'down':
+            this.y += STEP.Y;
+            if(this.y > START.X){
+                this.y = START.Y;
+            }
+            break;
+        case 'right':
+            this.x += STEP.X;
+            if(this.x > WINDOW_WIDTH - STEP.X / 5){
+                this.x = WINDOW_WIDTH - STEP.X;
+            }
+            break;
+        case 'up':
+            this.y -= STEP.Y;
+            if (this.y < 0) {
+                this.x = START.X;
+                this.y = START.Y;
+            }
+            break;
+    }
+};
+
+const player = new Player();
+function speed(){
+    var minVal = 60;
+    var maxVal = 200;
+    return Math.random() * (200 - 60) + 60;
+}
+
+function getEnemyRows() {
+    let rows = []
+    for(let i = 1; i <= 3; ++i)
+        rows.push(i * STEP.Y - STEP.X / 5);
+    return rows;
+}
+const enemyRows = getEnemyRows();
+
+function createEnemies() {
+    let enemies = [];
+    for(let i = 0; i < enemyRows.length; i++)
+        enemies.push( new Enemy(0, enemyRows[i], speed(), player));
+    return enemies;
+}
+const allEnemies = createEnemies();
+
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
